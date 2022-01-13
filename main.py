2f24165ea363eba177cf2e9387333e26e0ed9c29
JATafarelo <jguelfi@analytics2go.com>
2022-01-13 15:15:09 -0300

    grouping and modification

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import re
 
-test_string = '123abc456789abc123ABC'
+# test_string = '123abc456789abc123ABC'
+# test_string = 'hello 123_ heyho hohey'
+# test_string = 'helloHELLO 123-_'
+# test_string = 'hello_123'
 
 # a = '\tHello'
 # print(a)
@@ -9,18 +12,54 @@ test_string = '123abc456789abc123ABC'
 # a = r'\tHello'
 # print(a)
 
-# matches = re.finditer(r"abc",test_string)
-pattern = re.compile(r"abc")
-matches = pattern.finditer(test_string)
-# matches = pattern.findall(test_string)
+text = """
+Hello World!
+123456789
+2022-01-13
 
-for match in matches:
-    print(match.group())
+Mr Simpson
+Mrs Simpson
+Mr. Brown
+Ms Smith
+Mr. T
+
+firsttest@gmail.com
+second-test@gmx.de
+third-test123@my-domain.org
+
+"""
+
+# pattern = re.compile(r'(Mr|Mrs|Ms)\.?\s\w+')
+# pattern = re.compile(r'([a-zA-Z0-9-]+)@([a-zA-Z-]+)\.([a-zA-Z]+)')
+
+# matches = re.finditer(r"abc",test_string)
 
 # match = pattern.match(test_string)
 # match = pattern.search(test_string)
 
 # print(match)
 
+# matches = pattern.finditer(test_string)
+# matches = pattern.findall(test_string)
+
+# matches = pattern.finditer(text)
+
+# for match in matches:
+    # print(match.group(0))
+    # print(match.group(1))
+    # print(match.group(2))
+    # print(match.group(3))
+
+# test = '123abc456789abc123ABC'
+
+test = 'hello world, you are the best world'
+
+pattern = re.compile(r"world")
+
+# splitted = pattern.split(test)
+# print(splitted)
+
+sub = pattern.sub("planet",test)
+print(sub)
 
 
